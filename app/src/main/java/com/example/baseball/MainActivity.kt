fa52de5cabe87158514bad31ea8e5e2f9d1cd099
kimeunsur <124233661+kimeunsur@users.noreply.github.com>
2024-12-26 15:42:36 +0900

    base

--- app/src/main/java/com/example/baseball/MainActivity.kt
+++ app/src/main/java/com/example/baseball/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.baseball.databinding.ActivityMainBinding
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
